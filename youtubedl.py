9004d21a43f846f1141a3b8e0802ab897b7930b2
roro3828 <roro@roro3828.com>
2021-12-03 22:30:24 +0900

    python

---
+++ youtubedl.py
@@ -0,0 +1,11 @@
+import youtube_dl
+import sys
+from os.path import expanduser
+home=expanduser("~")
+
+ids=sys.argv[1].split(':')[1:]
+ydl_opts = {
+    'outtmpl':home+r'\Downloads\%(title)s-%(id)s.%(ext)s'
+    }
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download(ids)
